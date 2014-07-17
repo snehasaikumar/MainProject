603ef6b81eadbaf2cc1ad263279770a25b01c1f5
Sneha Saikumar <ssaikumar@hhogdev.com>
2014-07-17 17:13:52 -0400

    Updated sub and main

--- MainProject/Program.cs
+++ MainProject/Program.cs
@@ -13,6 +13,8 @@ namespace MainProject
         {
             Console.WriteLine("Hellooooo");
 
+
+            //test
             var calculate = new Calculate();
             Console.WriteLine("Adding numbers 1728 and 1: " + calculate.Add(1728, 1));
             Console.WriteLine("Subtracting numbers 1730 and 1: " + calculate.Subtract(1730, 1));
